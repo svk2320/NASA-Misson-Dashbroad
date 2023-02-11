f76e045f5de0e4253df2093b15628b2be3d3fe90
vasanth2401 <vasanthkumars2025@gmail.com>
2023-02-11 16:07:53 +0530

    copied build from frontend to backend

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -10,6 +11,12 @@ app.use(cors({
 }));
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '..', 'public','index.html'))
+})
 
 module.exports = app;
