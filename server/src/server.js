86da5470ef363ba92e6ced31f735c26674270a73
vasanth2401 <vasanthkumars2025@gmail.com>
2023-02-14 08:01:41 +0530

    mades small changes

--- server/src/server.js
+++ server/src/server.js
@@ -7,9 +7,9 @@ const { loadPlanetData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
-const PASSWORD = '4TQPT2abkgxOLvzE';
+const PASSWORD = 'WldOBTNZY6XcqqAz';
 
-const MONGO_URL = `mongodb+srv://nasa-appi:${PASSWORD}@nasa.u9gpflr.mongodb.net/nasa?retryWrites=true&w=majority`;
+const MONGO_URL = `mongodb+srv://NASA_API:${PASSWORD}@nasa-lachen-dashboard.jgczxdu.mongodb.net/?retryWrites=true&w=majority`;
 
 const server = http.createServer(app);
 
