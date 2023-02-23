a8674fc7c28e5604528935077d3b08023e561b58
vasanth2401 <vasanthkumars2025@gmail.com>
2023-02-23 18:16:28 +0530

    remove a line in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/', api);
-// app.use('/v2', apiforvs);
+app.use('/v2', apiforvs);
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
